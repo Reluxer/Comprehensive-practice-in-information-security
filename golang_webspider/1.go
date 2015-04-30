1846ea82e7b9ef63a9142b787aad864d55a3ce09
ToxicLibra <jxlux26@gmail.com>
2015-04-30 09:51:23 +0800

    new

--- golang_webspider/1.go
+++ golang_webspider/1.go
@@ -38,20 +38,20 @@ func GetPageToFile(url string, filename string) (ok bool) {
 	// if checkFileIsExist(filename) {
 
 	// f, err = os.OpenFile(filename, os.O_APPEND, 0666) //打开文件
-	//  fmt.Println("File exited,the original file will be overwritten")
-	//  if err != nil {
-	//      fmt.Println("Failed to open file. 0_0", filename)
-	//      return
-	//  }
-	//  fmt.Println("File has been opened...")
+	// 	fmt.Println("File exited,the original file will be overwritten")
+	// 	if err != nil {
+	// 		fmt.Println("Failed to open file. 0_0", filename)
+	// 		return
+	// 	}
+	// 	fmt.Println("File has been opened...")
 	// } else {
-	//  f, err = os.Create(filename) //创建文件
-	//  fmt.Println("File not exit, now creating it...")
-	//  if err != nil {
-	//      fmt.Println("Failed to create file. 0_0", filename)
-	//      return
-	//  }
-	//  fmt.Println("File created, prepare writing...")
+	// 	f, err = os.Create(filename) //创建文件
+	// 	fmt.Println("File not exit, now creating it...")
+	// 	if err != nil {
+	// 		fmt.Println("Failed to create file. 0_0", filename)
+	// 		return
+	// 	}
+	// 	fmt.Println("File created, prepare writing...")
 	// }
 
 	if f, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666); err != nil {
@@ -92,24 +92,20 @@ func Get(url string) (content string, statusCode int) {
 	return
 }
 
-func ParseLinks() {
-
-}
-
 // func checkFileIsExist(filename string) bool {
-//  var exist = true
-//  if _, err := os.Stat(filename); os.IsNotExist(err) {
-//      exist = false
-//  }
-//  return exist
+// 	var exist = true
+// 	if _, err := os.Stat(filename); os.IsNotExist(err) {
+// 		exist = false
+// 	}
+// 	return exist
 // }
 
 // func GetCurrPath() string {
-//  file, _ := exec.LookPath(os.Args[0])
-//  path, _ := filepath.Abs(file)
-//  splitstring := strings.Split(path, "\\")
-//  size := len(splitstring)
-//  splitstring = strings.Split(path, splitstring[size-1])
-//  ret := strings.Replace(splitstring[0], "\\", "/", size-1)
-//  return ret
+// 	file, _ := exec.LookPath(os.Args[0])
+// 	path, _ := filepath.Abs(file)
+// 	splitstring := strings.Split(path, "\\")
+// 	size := len(splitstring)
+// 	splitstring = strings.Split(path, splitstring[size-1])
+// 	ret := strings.Replace(splitstring[0], "\\", "/", size-1)
+// 	return ret
 // }
---
+++ golang_webspider/config/config.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	Url           = `https://github.com/ZJGSU-Open-Source/GoOnlineJudge`
+	FileStorePath = `.`
+)
