cfbb88341d3dd5e3a2c8a14ed2e19834e50721d3
Gxfary <reluxer@163.com>
2015-05-01 00:34:31 +0800

    something new

--- golang_webspider/spider.go
+++ golang_webspider/spider.go
@@ -8,19 +8,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+type URLMeta struct {
+	extension string
+	URL       string
+}
 
 func main() {
 
-	rootUrl := `http://www.bnuoj.com`
-	fileStorePath := `./`
-	logdb := `./visitedurl`
-	depth := 3
+	rootUrl := `https://www.baidu.com/`
+	depth := 2
 
-	if ok := GetPageToFile(depth, rootUrl, fileStorePath, logdb); !ok {
-		return
-	}
-
+	GetPageToFile(rootUrl, depth)
 }
 
 func GetHash(c string) (h string) {
@@ -30,7 +31,7 @@ func GetHash(c string) (h string) {
 	return h
 }
 
-func GetPageToFile(dep int, url, FilePath, Logfile string) (ok bool) {
+func GetPageToFile(url string, dep int) (ok bool) {
 
 	if dep < 0 {
 		// only crawl until depth has reached the bottom or zero
@@ -38,58 +39,97 @@ func GetPageToFile(dep int, url, FilePath, Logfile string) (ok bool) {
 		return
 	}
 
-	var f, flog *os.File
-	var err error
-	ok = false
-
-	content, statusCode := Get(url)
-	contentstr := string(content)
+	contentbyte, statusCode := Get(url)
+	contentstr := string(contentbyte)
 
 	if statusCode != 200 {
-		fmt.Println(statusCode, "Url error")
+		fmt.Println(statusCode, url)
 		return
 	}
 	// fmt.Println(statusCode)
 
-	name := GetHash(contentstr)
-	filename := FilePath + name + ".html"
+	fmt.Println("depth", dep, "Hit url:", url)
+
+	fileStorePath := `./`
+	logdb := `./visitedurl`
+
+	ok = str2file(contentstr, fileStorePath, logdb, url)
+
+	if !ok {
+		return ok
+	}
+
+	ParseUrls(contentbyte, dep)
+
+	ok = true
+	return ok
+}
+
+func str2file(c string, fp, logfile, url string) (ok bool) {
+	var f, flog *os.File
+	var err error
+
+	rx, err1 := regexp.Compile("(?:\\.)[[:alpha:]]+$")
+	if err1 != nil {
+		return ok
+	}
+
+	mx := rx.FindString(url)
+
+	fmt.Println("url:", url, "extenion:", mx)
+
+	name := GetHash(c)
+	filename := fp + name + mx
+
+	if checkFileIsExist(filename) {
+		ok = true
+		return ok
+	}
 
 	if f, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666); err != nil {
 		fmt.Println("Error:", err)
-		return
+		return ok
 	}
 
 	defer f.Close()
 
-	_, err = io.WriteString(f, contentstr) //写入文件(字符串)
+	_, err = io.WriteString(f, c) //写入文件(字符串)
 
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return ok
 	}
-	fmt.Println("Success! ^_^")
-	ok = true
 
-	if flog, err = os.OpenFile(Logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666); err != nil {
+	// fmt.Println("Success! ^_^")
+
+	if flog, err = os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666); err != nil {
 		fmt.Println(err)
-		ok = false
-		return
+		return ok
 	}
 
 	defer flog.Close()
 
-	_, err = io.WriteString(flog, url+"\t"+name+"\n")
+	_, err = io.WriteString(flog, url+"\t"+filename+"\n")
 
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return ok
 	}
+	ok = true
+	fmt.Println("Stored url:", url)
+	return ok
+}
 
-	return
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
 }
 
 func Get(url string) (data []byte, statusCode int) {
-	var errs, error
+	var errs error
 	resp, errf := http.Get(url)
 	if errf != nil {
 		statusCode = -100
@@ -106,9 +146,57 @@ func Get(url string) (data []byte, statusCode int) {
 	}
 	statusCode = resp.StatusCode
 	// content = string(data)
-	return
+	return data, statusCode
 }
 
-func ParseLinks() {
+func ParseUrls(bytes []byte, depth int) {
+	urlRx, err := regexp.Compile("https?://[[:alpha:]]+([.]?[[:alpha:]]+)*?(/+[[:alpha:]]+)+(/|(\\.[[:alpha:]]+))?")
+	if err != nil {
+		return
+	}
+	urls := urlRx.FindAll(bytes, -1)
+	urlCount := len(urls)
 
+	// create a channel to send all of the urls we encounter
+	urlChan := make(chan []byte, 10000)
+	for idx := 0; idx < urlCount; idx++ {
+		urlChan <- urls[idx]
+	}
+	close(urlChan)
+	urlpoor := getURLs(urlChan)
+
+	for m := range urlpoor {
+		GetPageToFile(m, depth-1)
+	}
+	// URLMetaPrinter(urlpoor, depth)
 }
+
+func getURLs(urls <-chan []byte) <-chan string {
+	out := make(chan string)
+	// rx, err := regexp.Compile("(?:\\.)[[:alpha:]]+$")
+	// if err != nil {
+	// 	return out
+	// }
+	go func() {
+		for url := range urls {
+			// for each url try to grab the extension
+			// match := rx.Find(url)
+			// mt := URLMeta{URL: string(url[:]), extension: string(match[:])}
+			out <- string(url[:])
+		}
+		close(out)
+	}()
+	return out
+}
+
+// func URLMetaPrinter(um <-chan string, depth int) {
+// 	for m := range um {
+// 		// if m.extension != "" {
+// 		// 	fmt.Println("depth:", depth, "URL:", m.URL, "extension:", m.extension)
+// 		// } else {
+// 		// 	fmt.Println("depth:", depth, "URL:", m.URL)
+// 		// }
+// 		// recursively crawl found urls
+// 		GetPageToFile(m.URL, depth-1)
+// 	}
+// }
